ec200fb4b5a5bec7d8263d58208d1e1e86ceb333
gouz7514 <gouz7514@gmail.com>
2023-03-30 22:58:38 +0900

    Update : sass 사용 취소

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.scss'
-import typoStyles from '@/styles/Typo.module.scss'
+import styles from '@/styles/Home.module.css'
+import typoStyles from '@/styles/Typo.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
