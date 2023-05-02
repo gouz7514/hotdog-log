0d3384c1635d77b09c40f22072c6c2e3c84d05c2
gouz7514 <gouz7514@gmail.com>
2023-05-02 17:14:38 +0900

    Add : resume page

---
+++ src/pages/resume/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Resume() {
+  return (
+    <>
+      <h1>
+        This is Resume page
+      </h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  )
+}
