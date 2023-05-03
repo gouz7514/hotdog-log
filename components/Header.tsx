bc2bafdce5290490a36efb1a7c06ff8af7f394f1
gouz7514 <gouz7514@gmail.com>
2023-05-03 18:55:20 +0900

    Update : 메인 페이지 대강 완성 및 페이지 구조 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 import styled from 'styled-components'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const StickyHeader = styled.div`
   position: sticky;
-  padding-top: 32px;
-  top: -32px;
-  background-color: #CDE4B4;
-  border-bottom: 1px black solid;
+  padding-top: 16px;
+  top: -16px;
+  box-shadow: 2px 4px 15px 0px rgba(0,0,0,0.75);
   width: 100%;
+  display: flex;
 `
 
 const CommonHeader = styled.div`
@@ -20,13 +21,24 @@ const CommonHeader = styled.div`
   height: 60px;
   align-items: center;
   padding: 8px;
+  width: 100%;
 
-  .header-links {
-    padding: 8px;
+  .header-pages {
+    display: flex;
 
-    &.active {
-      text-decoration: underline;
+    .header-links {
+      padding: 8px;
+  
+      &.active {
+        text-decoration: underline;
+      }
     }
+  }
+
+  .header-external {
+    margin-left: auto;
+    display: flex;
+    gap: 12px;
   }
 `
 
@@ -47,14 +59,34 @@ export default function Header({ page }: HeaderProps) {
   return (
     <StickyHeader>
       <CommonHeader>
-        <div className={`header-links ${conditionalClass('')}`}>
-          <Link href="/">Home</Link>
+        <div className="header-pages">
+          <div className={`header-links ${conditionalClass('')}`}>
+            <Link href="/">Home</Link>
+          </div>
+          <div className={`header-links ${conditionalClass('posts')}`}>
+            <Link href="/posts">Posts</Link>
+          </div>
+          <div className={`header-links ${conditionalClass('resume')}`}>
+            <Link href="/resume">Resume</Link>
+          </div>
         </div>
-        <div className={`header-links ${conditionalClass('posts')}`}>
-          <Link href="/posts">Posts</Link>
-        </div>
-        <div className={`header-links ${conditionalClass('resume')}`}>
-          <Link href="/resume">Resume</Link>
+        <div className="header-external">
+          <a href="https://github.com/gouz7514">
+            <Image
+              src="/images/logo-github.webp"
+              height={40}
+              width={40}
+              alt="github logo"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/%ED%95%99%EC%9E%AC-%EA%B9%80-a23a7b271">
+            <Image
+              src="/images/logo-linkedin.webp"
+              height={40}
+              width={40}
+              alt="linkedin logo"
+            />
+          </a>
         </div>
       </CommonHeader>
     </StickyHeader>
--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -3,12 +3,14 @@ import Link from 'next/link'
 export default function Resume() {
   return (
     <>
-      <h1>
-        This is Resume page
-      </h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <div className="container">
+        <h1>
+          This is Resume page
+        </h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </div>
     </>
   )
 }
