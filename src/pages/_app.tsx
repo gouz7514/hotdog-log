b0f755a4cd5a344180a6fca9c3003a7e43c333f5
gouz7514 <gouz7514@gmail.com>
2023-05-09 15:57:30 +0900

    Update : _document의 meta 속성을 _app 파일로 옮김

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <AppLayout page={page}>
       <Head>
         <title>게으른 개발자, 김학재입니다</title>
+        <meta name="description" content="김학재의 개발 블로그" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </AppLayout>
