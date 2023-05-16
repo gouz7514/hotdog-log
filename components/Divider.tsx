bd6f2e44feae851525770ea5c18035a28cbd6049
gouz7514 <gouz7514@gmail.com>
2023-05-16 22:14:25 +0900

    Add : Badge, Divider component

---
+++ components/Divider.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const DividerContainer = styled.div`
+  border: 1px solid #eeeeee;
+  width: 100%;
+  margin: 24px 0;
+`
+
+export default function Divider() {
+  return (
+    <DividerContainer />
+  )
+}
