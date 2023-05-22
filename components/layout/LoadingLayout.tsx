bd5d49aa9a0136fd6ba60ac51bf4ce09853d1c20
gouz7514 <gouz7514@gmail.com>
2023-05-22 19:34:31 +0900

    Add : framer loading animation between page

---
+++ components/layout/LoadingLayout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+import { motion } from 'framer-motion'
+
+type LoadingProps = {
+  children: ReactNode
+}
+
+export default function LoadingLayout({ children }: LoadingProps ) {
+  return (
+    <motion.div
+      initial={{ x: 300, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 300, opacity: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+      }}
+    >
+      {children}
+    </motion.div>
+  )
+}
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link'
+import LoadingLayout from "../../../components/layout/LoadingLayout"
 
 export default function Posts() {
   return (
-    <>
+    <LoadingLayout>
       <div className='container'>
         <h2>
           각종 공부한 내용들을 정리할 페이지입니다. Coming Soon
         </h2>
       </div>
-    </>
+    </LoadingLayout>
   )
 }
