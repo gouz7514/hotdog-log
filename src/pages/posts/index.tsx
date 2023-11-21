736608f67119971d3cdbc25fdfd38cfca71adc04
gouz7514 <gouz7514@gmail.com>
2023-11-22 00:27:23 +0900

    Fix : posts 페이지 레이아웃, 컴포넌트명 수정

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,16 +1,12 @@
-import LoadingLayout from "../../../components/layout/LoadingLayout"
-
 import AnimationHotdog from '../../../public/lottie/lottie-hotdog.json'
 import LottieAnimation from "../../../components/Lottie"
 
-export default function Projects() {
+export default function Posts() {
   return (
-    <LoadingLayout>
-      <div className='container'>
-        <div>
-          <LottieAnimation json={AnimationHotdog} description="아직 준비중인 페이지입니다. 조금만 기다려주세요!" />
-        </div>
+    <div className='container'>
+      <div>
+        <LottieAnimation json={AnimationHotdog} description="아직 준비중인 페이지입니다. 조금만 기다려주세요!" />
       </div>
-    </LoadingLayout>
+    </div>
   )
 }
