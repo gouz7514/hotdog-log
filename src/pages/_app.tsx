62e1514e50adad1ec0d4bf9ac571263e938a99cb
gouz7514 <gouz7514@gmail.com>
2023-11-21 19:10:06 +0900

    Update : meta tag, og_image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,12 +19,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>핫재의 개발 블로그</title>
         <meta name="title" content="핫재의 개발 블로그" />
         <meta name="description" content="김학재의 개발 블로그입니다" />
-        <meta name="og:title" content="핫재의 개발 블로그" />
-        <meta name="description" content="김학재의 개발 블로그입니다" />
-        <meta name="og:description" content="김학재의 개발 블로그입니다" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="핫재의 개발 블로그" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://hotjae.com" />
+        <meta property="og:description" content="김학재의 개발 블로그입니다" />
+        <meta property="og:image" content="https://hotjae.com/images/og_image.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image" content="/images/profile.webp" />
       </Head>
       <RecoilRoot>
         <AppLayout page={page}>
