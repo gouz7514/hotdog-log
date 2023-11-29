53021f5449a5e307ed19bdce21f9df6c9edf234d
gouz7514 <gouz7514@gmail.com>
2023-11-29 18:25:45 +0900

    Chore : remove console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,9 +28,7 @@ export const ThemeContext = createContext<ContextProps>({
 
 export default function App({ Component, pageProps }: AppProps) {
   const { colorTheme, toggleTheme } = useDarkMode()
-  RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false
-  // console.log('theme : ', theme)
-  console.log(colorTheme)
+  RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false  
 
   return (
     <>
