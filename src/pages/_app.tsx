130b134541bf7687255bcb54af60f39b7b7ea7c2
gouz7514 <gouz7514@gmail.com>
2023-11-30 17:52:32 +0900

    Remove : 불필요한 공백 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,7 +28,7 @@ export const ThemeContext = createContext<ContextProps>({
 
 export default function App({ Component, pageProps }: AppProps) {
   const { colorTheme, toggleTheme } = useDarkMode()
-  RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false  
+  RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false
 
   return (
     <>
