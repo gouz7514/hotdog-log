b5a4475763e4df728c65758063cfca3fa108aae7
gouz7514 <gouz7514@gmail.com>
2023-12-04 15:49:30 +0900

    Style : post item margin-bottom

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -32,7 +32,7 @@ const PostStyle = styled.div`
     border-bottom-right-radius: 12px;
     background-color: var(--color-list-background);
     list-style: none;
-    margin-bottom: 24px;
+    margin-bottom: 12px;
     padding: 12px 16px;
     box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
 
