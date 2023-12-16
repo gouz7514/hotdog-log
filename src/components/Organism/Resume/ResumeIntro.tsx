9c1ccdd01bc0f2b07bcc7cd3c9c7846c51076cab
gouz7514 <gouz7514@gmail.com>
2023-12-16 19:29:38 +0900

    Update : 이력서 페이지 컨텐츠 업데이트

--- src/components/Organism/Resume/ResumeIntro.tsx
+++ src/components/Organism/Resume/ResumeIntro.tsx
@@ -1,22 +1,25 @@
-import { Fragment } from 'react'
-
 import styled from '@emotion/styled'
 
 import Badge from '@/components/Molecule/Badge'
 
 const ResumeIntroStyle = styled.div`
-  margin-bottom: 12px;
+  .badge-place {
+    margin-top: 12px;
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+  }
 `
 
 export default function ResumeIntro() {
   return (
-    <>
+    <ResumeIntroStyle>
       <h2>
         프론트엔드 개발자,
         <br />
-        김학재입니다
+        <span className="text-blue">김학재</span>입니다
       </h2>
-      <ResumeIntroStyle>
+      <>
         <div className="big-paragraph">
           스타트업에서 웹 서비스를 개발/배포/운영한 경험이 있습니다.
           프론트엔드뿐 아니라 다양한 도메인에 걸쳐 서비스를 전반적으로 유지하고
@@ -28,13 +31,23 @@ export default function ResumeIntro() {
           불편해합니다. 서비스에 대한 책임감과 이해도를 바탕으로 항상 고민하며,
           주도적으로 솔루션을 제시해 해결하기 위해 노력합니다.
         </div>
-      </ResumeIntroStyle>
+        <div className="big-paragraph mt-12">
+          프로덕트의 개발 및 유지와 더불어 CS 응대와 같은 다양한 경험을 통해
+          프론트엔드 개발자로서 성장해 왔습니다. 이와 함께 블로그, 오픈 소스
+          기여, 프로젝트를 통해 성장하려는 자세를 바탕으로 부피뿐 아니라
+          밀도있는 개발자로 성장해나가고 있습니다.
+        </div>
+      </>
       <div className="badge-place">
         <Badge
           content="PDF로 보기"
-          link="https://drive.google.com/file/d/1FQ1IfrqY1Yn7ItXUC9ieULRjAL_mZUPW/view?usp=sharing"
+          link="https://drive.google.com/file/d/1qQLEBNZZ_OBjmTn8xaV8y1ZidKcd6gNV/view"
+        />
+        <Badge
+          content="경력기술서"
+          link="https://drive.google.com/file/d/1ibxaYoEZ6FYqsuNuOjTTQ2iF-ovQuTxw/view?usp=sharing"
         />
       </div>
-    </>
+    </ResumeIntroStyle>
   )
 }
