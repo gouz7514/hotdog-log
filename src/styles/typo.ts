7a7a69df10e257804b4fa412d89476dc3b72e600
gouz7514 <gouz7514@gmail.com>
2024-06-11 01:26:56 +0900

    Style : typo font size 반응형

--- src/styles/typo.ts
+++ src/styles/typo.ts
@@ -1,26 +1,30 @@
 import { css } from '@emotion/react'
 
 const typo = css`
+  html {
+    font-size: 16px;
+  }
+
   h1 {
-    font-size: 3em;
+    font-size: 3rem;
     margin: 24px 0;
   }
 
   h2 {
+    font-size: 2rem;
     margin: 12px 0;
-    font-size: 1.8em;
   }
 
   h3 {
-    font-size: 1.5em;
+    font-size: 1.5rem;
   }
 
   h4 {
-    font-size: 1.3em;
+    font-size: 1.3rem;
   }
 
   h5 {
-    font-size: 1.1em;
+    font-size: 1.1rem;
     line-height: 1.5;
   }
 
@@ -44,6 +48,12 @@ const typo = css`
 
   .text-orange {
     color: var(--color-orange);
+  }
+
+  @media screen and (max-width: 600px) {
+    html {
+      font-size: 14px;
+    }
   }
 `
 
