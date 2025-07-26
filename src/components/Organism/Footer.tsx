15b24c7ef4f78b64ed13a504142699b45d3e24e5
gouz7514 <gouz7514@gmail.com>
2025-07-27 01:47:01 +0900

    Feat : Footer에 X 추가

--- src/components/Organism/Footer.tsx
+++ src/components/Organism/Footer.tsx
@@ -1,26 +1,11 @@
-import styled from '@emotion/styled'
-import { useContext } from 'react'
 import ThemeContext from '@/context/themeContext'
 import { theme } from '@/styles/theme'
+import styled from '@emotion/styled'
+import { useContext } from 'react'
 
 import { Icon } from '../Atom'
 
-import { IconGithub, IconLinkedIn, IconGmail, IconVelog } from '../Icon'
-
-const AppFooter = styled.footer`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  position: relative;
-  bottom: 2rem;
-  width: 100%;
-`
-
-const ProfileLogo = styled.div`
-  margin-top: 40px;
-  display: flex;
-  gap: 24px;
-`
+import { IconGithub, IconGmail, IconLinkedIn, IconX } from '../Icon'
 
 export function Footer() {
   const { colorTheme } = useContext(ThemeContext)
@@ -41,10 +26,26 @@ export function Footer() {
         <a href="mailto:gouz7514@gmail.com" target="blank">
           <Icon icon={<IconGmail isDark={isDark} />} />
         </a>
-        <a href="https://velog.io/@gouz7514" target="blank">
-          <Icon icon={<IconVelog isDark={isDark} />} />
+        <a href="https://x.com/gouz7514" target="blank">
+          <Icon width={24} height={24} icon={<IconX isDark={isDark} />} />
         </a>
       </ProfileLogo>
     </AppFooter>
   )
 }
+
+const AppFooter = styled.footer`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  bottom: 2rem;
+  width: 100%;
+`
+
+const ProfileLogo = styled.div`
+  margin-top: 40px;
+  display: flex;
+  align-items: center;
+  gap: 24px;
+`
