1e438fedb51cf493f57d51215b7544144eed747a
gouz7514 <gouz7514@gmail.com>
2026-08-10 12:31:44 +0900

    refactor : dynamic import lottie-react

--- src/components/Organism/Lottie.tsx
+++ src/components/Organism/Lottie.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled'
-import Lottie from 'lottie-react'
+import dynamic from 'next/dynamic'
+
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false })
 
 interface LottieProps {
   json: Record<string, unknown>
