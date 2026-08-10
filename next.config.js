310534daf2019d82767056380fa24e2a5893ea98
gouz7514 <gouz7514@gmail.com>
2026-08-10 12:32:08 +0900

    chore : add images qualities config in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  images: {
+    qualities: [20, 40, 60, 80, 100],
+  },
   i18n: {
     locales: ['ko', 'en'],
     defaultLocale: 'ko',
